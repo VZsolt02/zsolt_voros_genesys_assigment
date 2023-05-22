0d1ea0d7ed9ff69bf2f5aa048adae42607a847b4
ZsoltVoros <YF8F3D@inf.elte.hu>
2023-05-22 14:50:14 +0200

    master: initialization

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static HashMap<Character, String> initDigitToLettersMap() {
+        HashMap<Character, String> digitToLettersMap = new HashMap<>();
+        digitToLettersMap.put('2', "abc");
+        digitToLettersMap.put('3', "def");
+        digitToLettersMap.put('4', "ghi");
+        digitToLettersMap.put('5', "jkl");
+        digitToLettersMap.put('6', "mno");
+        digitToLettersMap.put('7', "pqrs");
+        digitToLettersMap.put('8', "tuv");
+        digitToLettersMap.put('9', "wxyz");
+
+        return digitToLettersMap;
+    }
+
+    public static void main( String[] args ) {
+
+        HashMap<Character, String> digitToLettersMap = initDigitToLettersMap();
+        ArrayList<String> combinations = new ArrayList<>();
+
+        String testString = "ab";
+
+        
+
+
+
+
+
+    }
+}
